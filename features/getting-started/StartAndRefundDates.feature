6fc0cfd70ac32b8a92301a2654d9910f1b2dbd0f
abhi33333 <abhilasha.rana@gmail.com>
2026-07-08 18:20:47 -0400

    SEP Automation

---
+++ features/getting-started/StartAndRefundDates.feature
@@ -0,0 +1,20 @@
+@sep11
+Feature: Program start dates and Refund dates
+
+    As a customer, I want to see the program start dates and refund policy details before enrolling
+    so that I can make informed decisions.
+
+    #* AC1: Program Start date and refund dates must be displayed in Step 1 in Test Automation with Selenium Program.
+
+    Background:
+        Given user is already on enrollment page
+
+    
+    Scenario: Verify that program start date and refund date are displayed in step one
+        Then the program start date is displayed.
+        And the refund date is displayed.
+
+  
+    Scenario: Verify that program start date and refund date are correct
+        Then the displayed program start date is correct
+        And the displayed refund date is correct
---
+++ steps/getting-started/startAndRefundDates.js
@@ -0,0 +1,29 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect} from "@playwright/test";
+import { startApplicationPage, page } from "../../globalPagesSetup.js";
+import { productInfo } from "../../utilities/qa-data-reader.js";
+
+Given('user is already on enrollment page', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the program start date is displayed.', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the refund date is displayed.', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the displayed program start date is correct', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('the displayed refund date is correct', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
